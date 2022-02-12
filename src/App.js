5be83059ff946eacc99235b407c432284e8720d4
shrued <shrutisara10@gmail.com>
2022-02-12 17:37:13 +0530

    Working React + GPT3 story

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const { Configuration, OpenAIApi } = require("openai");
 
 function App() {
+  const [input, setInput] = useState("Input");
+  const [response, setResponse] = useState("Response");
+
+  const API_KEY = process.env.REACT_APP_OPENAI_API_KEY;
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target),
+      formDataObj = Object.fromEntries(formData.entries());
+    console.log(formDataObj.genre);
+
+    const configuration = new Configuration({
+      apiKey: API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    openai
+      .createCompletion("text-davinci-001", {
+        prompt: `Write a ${formDataObj.genre} short story:`,
+        temperature: 1,
+        max_tokens: 1000,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      })
+      .then((response) => {
+        setInput(formDataObj.genre);
+        setResponse(response.data.choices[0].text);
+      });
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <form onSubmit={onFormSubmit}>
+        <label>
+          Enter {/* prompt */} genre
+          <input type="text" name="genre" />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <div>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Input: {input} <br />
+          Response: {response}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
