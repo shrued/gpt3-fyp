79b8c395adbc95e6a297cf6bd437df9490ab0eae
shrued <shrutisara10@gmail.com>
2022-02-21 18:41:01 +0530

    Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import Layout from "./components/layout";
+import Home from "./components/home";
 import Story from "./components/story";
+import Bot from "./components/bot";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 export default function React() {
   return (
     <>
       <Layout>
-        <Story />
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/story" element={<Story />} />
+            <Route path="/bot" element={<Bot />} />
+          </Routes>
+        </Router>
       </Layout>
     </>
   );
